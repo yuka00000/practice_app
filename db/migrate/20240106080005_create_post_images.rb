470b84477f2ce45cde03e4c9449ddb8e7eb196ef
yuka00000 <yukayamada0914@gmail.com>
2024-01-06 10:07:59 +0000

    ブログ作成

--- db/migrate/20240106080005_create_post_images.rb
+++ db/migrate/20240106080005_create_post_images.rb
@@ -1,9 +1,8 @@
 class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
-      t.string :shop_name
-      t.text :caption
-      t.integer :user_id
+      t.string :title
+      t.text :body
       t.timestamps
     end
   end
